f015a379c0dae235d624da05ba6798806b1871a1
snahmad <naeem_fast@yahoo.com>
2020-04-27 17:52:08 +0100

    First version

--- test.cpp
+++ test.cpp
@@ -3,7 +3,7 @@
 #include "PlusOp.h"
 #include "MinusOp.h"
 
-TEST(AST, TestSingleTerminal) 
+TEST(AST, EvaluateSingleTerminal) 
 {
   
    Terminal terminal(2);
@@ -13,7 +13,7 @@ TEST(AST, TestSingleTerminal)
 
 }
 
-TEST(AST, TestSimplePlusExpression)
+TEST(AST, EvaluateSimplePlusExpression)
 {
     auto terminal_left = new Terminal(3);
     auto terminal_right = new Terminal(2);
@@ -24,13 +24,28 @@ TEST(AST, TestSimplePlusExpression)
 
 }
 
-TEST(AST, TestSimpleMinusExpression)
+TEST(AST, EvaluateSimpleMinusExpression)
 {
     auto terminal_left = new Terminal(9);
     auto terminal_right = new Terminal(14);
     MinusOp minus_op(terminal_left, terminal_right);
 
     EXPECT_EQ(-5, minus_op.evaluate());
+
+
+}
+
+TEST(AST, EvaluatePlusAndMinusExpression)
+{
+    auto terminal_left_for_plus = new Terminal(3);
+
+    auto terminal_left_for_minus = new Terminal(8);
+    auto terminal_right_for_minus = new Terminal(17);
+    auto minus_op = new MinusOp(terminal_left_for_minus, terminal_right_for_minus);
+
+    PlusOp expression(terminal_left_for_plus, minus_op);
+
+    EXPECT_EQ(-6, expression.evaluate());
 
 
 }
